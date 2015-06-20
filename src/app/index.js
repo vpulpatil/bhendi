817d2ca04c5e615459b2530adffe820adc9fb611
exp10r3r <kaushal.rajkotia@firstcry.com>
2015-06-20 14:03:00 +0530

    a little refactor

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,12 @@
 'use strict';
 
 angular.module('bhendi', ['ngAnimate', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ngMaterial'])
-        .config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
+        .config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider, $locationProvider) {
             $stateProvider
                     .state('home', {
                         templateUrl: 'app/components/navbar/navbar.html',
                         controller: 'NavbarCtrl',
                         abstract: true
-                    })
-                    .state('home.main', {
-                        url: '/',
-                        views: {
-                            '': {
-                                templateUrl: 'app/main/main.html',
-                                controller: 'MainCtrl'
-                            },
-                            'heading': {
-                                template: 'Dashboard'
-                            }
-                        }
                     });
             $urlRouterProvider.otherwise('/');
             $mdIconProvider
