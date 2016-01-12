dda898863cd99184ca9b264a5e93ebe62e6ef10e
Chaitanya Jadhav <cjadhav7477@gmail.com>
2016-01-13 01:45:45 +0530

    SideNav: Added expand and collapse code

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -38,8 +38,94 @@ angular.module('bhendi')
                         {
                             link: 'home.board',
                             title: 'Bulletin Board'
+                        },
+
+                    ];
+
+            $scope.Events_subMenu =
+                    [
+                        {
+                            link:'home.eventsWorkshops',
+                            title:'Workshops'
+                        },
+                        {
+                            link:'home.eventsGuestLectures',
+                            title:'Guest Lectures'
                         }
                     ];
+
+            $scope.TT_subMenu =
+                    [
+                        {
+                            link:'home.TTexam',
+                            title:'Exam TT'
+                        },
+                        {
+                            link:'home.TTregular',
+                            title:'Regular Lectures TT'
+                        }
+                    ];
+
+            $scope.BB_subMenu =
+                    [
+                        {
+                            link:'home.BBtnpc',
+                            title:'TNPC'
+                        },
+                        {
+                            link:'home.BBdept',
+                            title:'Departmental Boards'
+                        },
+                        {
+                            link:'home.BBstudentSection',
+                            title:'Student Section'
+                        },
+                        {
+                            link:'home.BBnccSports',
+                            title:'NCC & Sports'
+                        },
+                        {
+                            link:'home.BBexamSection',
+                            title:'Examination Section'
+                        },
+                    ];
+
+            $scope.BB_dept_subMenu =
+                    [
+                        {
+                            link:'home.BB_dept_Computer',
+                            title:'Computer Engineering'
+                        },
+                        {
+                            link:'home.BB_dept_IT',
+                            title:'Information Technology'
+                        },
+                       {
+                            link:'home.BB_dept_mechanical',
+                            title:'Mechanical Engineering'
+                        },
+                        {
+                            link:'home.BB_dept_extc',
+                            title:'EXTC'
+                        },
+                        {
+                            link:'home.BB_dept_chemical',
+                            title:'Chemical Engineering'
+                        },
+                        {
+                            link:'home.BB_dept_petro',
+                            title:'Petrochemical Engineering'
+                        },
+                        {
+                            link:'home.BB_dept_electrical',
+                            title:'Electrical Engineering'
+                        },
+                        {
+                            link:'home.BB_dept_civil',
+                            title:'Civil Engineering'
+                        },
+                    ];
+
 
             $scope.goto = function (state) {
                 $log.debug(state);
